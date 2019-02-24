3f552d5a822262f10eaa04d3805902933861637d
Katarzyna Nyczka <nyczka@free-dom.pl>
2019-02-24 19:39:30 +0100

    Created classes Product, Shop, go_script changed

--- go_script.py
+++ go_script.py
@@ -4,46 +4,57 @@ from shoper import Product, Shop
 def main():
 
     def go_on():
-        input("Enter - kontynuacja programu")
+        input("======= Enter - kontynuacja programu =========")
 
-    def create_product(p, msg):
+    def create_product(p):
         el_gener = (i for i in p)
         try:
-            print(f"\n{msg}\n")
-            Product(el_gener, el_gener, el_gener, el_gener)
+            Product(next(el_gener), next(el_gener), next(el_gener), next(el_gener))
         except ValueError as err:
             print(err.args[0], '\n')
         except KeyError as err:
             print(err.args[0], '\n')
+        else:
+            print("Produkt dodano!\n")
         finally:
             print(Product.show_products())
 
-    print("\nTworzę kilka produktów... \n")
+    def add_products_to_shop(shop, p, quantity):
+        try:
+            shop.add_product(p, quantity)
+        except KeyError as err:
+            print(err.args[0], '\n')
+        else:
+            print("\nDodano produkt\n")
+
+    # ---------------------------------------------
+
+    print("\n1. Tworzę kilka produktów... \n")
     p1 = Product("tv_sam_s23", "Samsung S23 tv set", 1000.00, 3.00)
     p2 = Product("phone_lgv30", "Smartphone LG V30", 3000.00, 0.2)
     p3 = Product("tv_sam_s21", "SamsungS21 tv set", 990.00, 3.00)
     p4 = Product("phone_s23", "Smartphone Samsung SG33", 2500.00, 0.2)
     p5 = Product("phone_htc10", "Smartphone HTC 10", 2700.00, 0.2)
+    print(Product.show_products())
 
-    msg = "Próba dodania produktu z istniejącym już kodem\n"
+    print("\n2.a. Próba dodania produktu z istniejącym już kodem:\n")
     p = ["phone_htc10", "Smartphone HTC 10", 2700.00, 0.2]
+    create_product(p)
+    go_on()
 
-    create_product(p, msg)
+    print("\n2.b. Próba dodania produktu ze złym typem danych w polu Price:\n")
+    p = ["ble", "Ble", 'aaaa', 0.2]
+    create_product(p)
+    go_on()
 
-    # try:
-    #     print("\nPróba dodania produktu z tym samym kodem... \n")
-    #     Product("tv_sam_s23", "Samsung S23 tv set", 1000.00, 3.00)
-    # except ValueError as err:
-    #     print(err.args[0], '\n')
-    # except KeyError as err:
-    #     print(err.args[0], '\n')
-    # finally:
-    #     print(Product.show_products())
-
+    print("\n2.c. Próba dodania nowego produktu z prawidłowymi danymi:\n")
+    p = ["latitude e7470", "Laptop Dell", 3000.00, 1.0]
+    print(p)
+    create_product(p)
     go_on()
 
     # Utworzenie nowego sklepu
-    print("\nTworzenie nowego sklepu")
+    print("\n3. Tworzenie nowego sklepu")
     shop_name = input("Podaj nazwę sklepu: ")
     new_shop = Shop(shop_name)
 
@@ -53,23 +64,26 @@ def main():
     new_shop.add_product(p3, 15)
 
     # Lista produktów w sklepie
-    print(f"W sklepie {new_shop.name} są następujące produkty: \n")
+    print(f"4. Do sklepu {new_shop.name} dodano następujące produkty: \n")
     new_shop.show_products_list()
 
-
     # Próba dodania tego samego produktu
-    print("\nPróbujemy dodać istniejący już w sklepie produkt")
-    try:
-        new_shop.add_product(p3, 20)
-    except ValueError("Produkt istnieje już w sklepie!\n"):
-        print("Produkt nie został dodany")
-    else:
-        print("Dodano produkt")
+    print("\n4.a Próbujemy dodać istniejący już w sklepie produkt\n")
+    add_products_to_shop(new_shop, p1, 20)
+    go_on()
+
+    # Próba dodania nowego produktu
+    print("\n4.b Próbujemy dodać nowy produkt do sklepu\n")
+    print(p4.__repr__())
+    add_products_to_shop(new_shop, p4, 20)
+    go_on()
 
     # Zmiana ilości produktów
+    print("\n5. Próbujemy zmienić pole quantity w jednym z produktów w sklepie\n")
+    new_shop.show_product(p1)
     new_quantity = 20
     product = p1
-    print(f"\nZmieniamy ilość jednego z produktów w sklepie: {product} na {new_quantity} szt")
+    print(f"\nZmieniamy ilość na {new_quantity} szt")
     new_shop.change_quantity(product, new_quantity)
     print("\nAktualna lista produktów w sklepie: ")
     new_shop.show_products_list()
--- shoper.py
+++ shoper.py
@@ -4,7 +4,7 @@ class Product():
 
     def check_code(code):
         if code in Product.product_list.keys():
-            raise KeyError('Kod musi być unikalny!')
+            raise KeyError('Not a proper code! The code already in the base!')
 
     def __init__(self, code, name, price, weight):
         self.__code = code
@@ -13,20 +13,19 @@ class Product():
         self.weight = weight
         Product.check_code(self.__code)
         if not isinstance(self.name, str):
-            raise ValueError("Not a proper name")
+            raise ValueError("Not a proper name!\nProduct wasn't added")
         if not isinstance(self.price, float):
-            raise ValueError("Not a proper value")
+            raise ValueError("Not a proper value!\nProduct wasn't added")
         if not isinstance(self.weight, float):
-            raise ValueError("Not a proper value")
+            raise ValueError("Not a proper value!\nProduct wasn't added")
         Product.product_list[self.__code] = (self.name, self.price, self.weight)
 
     def __str__(self):
         return "Produkt: " + self.__code + " - " + self.name
 
+    def __repr__(self):
+        return self.__code + ' - ' + self.name + ' - ' + str(self.price)
 
-    # @staticmethod
-    # def show_codes():
-    #     print(Product.products_codes)
 
     @classmethod            # jaka jest różnica między staticmethod a classmethod
     def show_codes(cls):
@@ -34,7 +33,7 @@ class Product():
 
     @staticmethod
     def show_products():
-        print(f"Utworzono {len(Product.product_list)} produktów:")
+        print(f"Lista utworzonych produktów (pozycji: {len(Product.product_list)})")
         no = 1
         for code, product in Product.product_list.items():
             print(f"{no}, {code}, {product}")
@@ -46,10 +45,10 @@ class Shop():
     def __init__(self, name="No name shop"):
         self.name = name
         self.products_in_shop = {}
-        print(f"Stworzono nowy sklep: {name}\n")
+        print(f"New shop was created: {name}\n")
 
     def __str__(self):
-        return f"Nazwa sklepu: {self.name}\n"
+        return f"Shop's name: {self.name}\n"
 
 
     def add_product(self, product, quantity):
@@ -58,9 +57,8 @@ class Shop():
         if self.product not in self.products_in_shop:
             self.products_in_shop[self.product] = self.quantity
         else:
-            raise ValueError(f"{self.product} został już dodany do sklepu!")
+            raise KeyError(f"{self.product} already in the {self.name} shop!")
 
-            # print(f"{self.product} znajduje się już w sklepie!\nProduktu nie dodano")
 
     def change_quantity(self, product, quantity):
         self.product = product
@@ -74,6 +72,14 @@ class Shop():
     def show_products_list(self):
         for self.product, self.quantity in self.products_in_shop.items():
             print(f"{self.product}, ilość: {self.quantity}")
+
+    def show_product(self, product):
+        self.product = product
+        self.quantity = self.products_in_shop.get(self.product, 'No such product')
+        print(f"{self.product}, {self.quantity} szt")
+
+
+
 
 
 
